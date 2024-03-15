4f171950c970680a0dfbe0c26a0134270f55df5d
Rajat Jindal <rajatjindal83@gmail.com>
2024-03-15 07:33:08 +0530

    fixup
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- crates/containerd-shim-wasm/src/sys/unix/metrics.rs
+++ crates/containerd-shim-wasm/src/sys/unix/metrics.rs
@@ -2,10 +2,12 @@ use anyhow::Result;
 use containerd_shim::cgroup::collect_metrics;
 use containerd_shim::util::convert_to_any;
 use protobuf::well_known_types::any::Any;
+use log::info;
 
 pub fn get_metrics(pid: u32) -> Result<Any> {
+    info!("inside get metrics fn");
     let metrics = collect_metrics(pid)?;
-
+    info!("after collect metrics inside get metrics fn");
     let metrics = convert_to_any(Box::new(metrics))?;
     Ok(metrics)
 }
